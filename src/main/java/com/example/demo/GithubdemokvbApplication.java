f8c08ef2b8b4168ff2613a3ab5fe6f41989539b5
krishnavenikvb <krishnaveni.kvb69@gmail.com>
2023-03-15 17:40:24 +0530

    initial commit kvb

---
+++ src/main/java/com/example/demo/GithubdemokvbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubdemokvbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubdemokvbApplication.class, args);
+	}
+
+}
